47f736174798acb5711f592998d06d7153bd015b
egoisticfool <50340628+egoisticfool@users.noreply.github.com>
2019-05-07 14:46:38 +0600

    Add files via upload

---
+++ UVA 11854_Egypt.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    int arr[3];
+    while(cin>>arr[0]>>arr[1]>>arr[2])
+    {
+        if(arr[0]==0 && arr[1]==0 && arr[2]==0) break;
+        sort(arr, arr+3);
+        if((arr[0]*arr[0])+(arr[1]*arr[1])==(arr[2]*arr[2])) cout<<"right"<<endl;
+        else cout<<"wrong"<<endl;
+    }
+    return 0;
+}
