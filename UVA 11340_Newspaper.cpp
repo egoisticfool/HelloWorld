ef6a5ce403d6e22a02f1206935c10f4b8f84c9e0
egoisticfool <50340628+egoisticfool@users.noreply.github.com>
2019-05-30 01:14:17 +0600

    Add files via upload

---
+++ UVA 11340_Newspaper.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<iomanip>
+
+using namespace std;
+
+typedef pair<char, int>pr;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    cout<<setprecision(2);
+    cout<<fixed;
+    int test, w, lines;
+    cin>>test;
+    cin.ignore();
+    while(test--)
+    {
+        cin>>w;
+        char ch;
+        string s[150005];
+        vector<pr>vec;
+        double value;
+        cin.ignore();;
+        for(int i = 0; i<w; i++)
+        {
+            cin>>ch>>value;
+            vec.push_back(make_pair(ch, value));
+        }
+        cin.ignore();
+        cin>>lines;
+        cin.ignore();
+        for(int i = 0; i<lines; i++)
+        {
+            getline(cin, s[i]);
+        }
+        double sum = 0;
+        for(int i = 0; i<lines; i++)
+        {
+            for(int j = 0; j<s[i].size(); j++)
+            {
+                char check = s[i][j];
+                for(int k = 0; k<vec.size(); k++)
+                {
+                    if(check==vec[k].first)
+                    {
+                        sum+=vec[k].second;
+                        break;
+                    }
+                }
+            }
+        }
+        cout<<sum/100.0<<"$"<<endl;
+        vec.clear();
+    }
+    return 0;
+}
